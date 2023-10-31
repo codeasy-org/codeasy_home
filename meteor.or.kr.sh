6dae2f5f58feb342abe1dd49dd7c43cbcdf5f15e
Ubuntu <ubuntu@ip-172-31-2-209.ap-northeast-2.compute.internal>
2023-10-31 19:49:10 +0900

    .

--- meteor.or.kr.sh
+++ meteor.or.kr.sh
@@ -1,9 +1,9 @@
 export MONGO_URL=mongodb://localhost:27777/codeasy_home
 export MAIL_URL=smtps://meteorstartup:kwlgevradebyrszc@smtp.gmail.com:465
-export ROOT_URL=http://codeasy.org
-export PORT=3102
+export ROOT_URL=http://meteor.or.kr
+export PORT=3106
 
 #meteor -p3102
 #meteor node .meteor/build/bundle/main.js --codeasy=codeasy_home
 METEOR_NODE_PATH=$(meteor node -e "console.log(process.execPath);" 2>&1)
-pm2 start .meteor/local/___build/bundle/main.js --name "codeasy_home" --interpreter "$METEOR_NODE_PATH"
+pm2 start .meteor/build/bundle/main.js --name "meteor.or.kr" --interpreter "$METEOR_NODE_PATH"
